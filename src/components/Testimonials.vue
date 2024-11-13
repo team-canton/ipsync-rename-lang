18adfa6405744086c95cfd0ab1059b36ebd377b0
Mark Andrei Encanto <markandrei.encanto@wvsu.edu.ph>
2024-11-13 17:21:07 +0800

    Create Testimonials.vue

---
+++ src/components/Testimonials.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="">
+        <div>
+            hi
+        </div>
+        <div>
+            <div class="text-[#666262] text-[1rem]">
+                
+            </div>
+        </div>
+    </div>
+</template>
